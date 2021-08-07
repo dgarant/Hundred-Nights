e1dc075f21ec05b487fb958f2bbe7e5cf3490f41
Dan Garant <dan@garantanalytics.com>
2021-08-07 00:39:48 +0000

    Add logging to file

--- HundredNights/settings.py
+++ HundredNights/settings.py
@@ -155,9 +155,6 @@ INSTALLED_APPS = (
     'south'
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
@@ -169,16 +166,18 @@ LOGGING = {
         }
     },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
+        'file': {
+            'level': 'DEBUG',
+            'class' : 'logging.handlers.RotatingFileHandler',
+            'filename': '/var/www/Hundred-Nights/applog.txt',
+            'maxBytes' : 1024*1024*10, # 10MB
+            'backupCount' : 2
         }
     },
     'loggers': {
         'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
+            'handlers': ['file'],
+            'level': 'DEBUG',
             'propagate': True,
         },
     }
